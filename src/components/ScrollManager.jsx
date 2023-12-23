65270b5f22a572ae4dcda27aa06b2f9ba1ae3706
surepos <125876053+surepos@users.noreply.github.com>
2023-12-23 23:55:55 +0000

    Update ScrollManager.jsx

--- src/components/ScrollManager.jsx
+++ src/components/ScrollManager.jsx
@@ -10,8 +10,23 @@ export const ScrollManager = (props) => {
   const lastScroll = useRef(0);
   const isAnimating = useRef(false);
 
-  data.fill.classList.add("top-0");
-  data.fill.classList.add("absolute");
+  data.fill.classList.add("fix");
+
+  useEffect(() => {
+    function onScroll(event) {
+      if (isAnimating.current) {
+        return;
+      }
+      isAnimating.current = true;
+      if (event.deltaY < 0) {
+        onSectionChange((section) => (section > 0 ? section - 1 : 0));
+      } else if (event.deltaY > 0) {
+        onSectionChange((section) => (section === 3 ? 3 : section + 1));
+      }
+    }
+    window.addEventListener("wheel", onScroll);
+    return () => window.removeEventListener("wheel", onScroll);
+  }, []);
 
   useEffect(() => {
     gsap.to(data.el, {
@@ -25,25 +40,6 @@ export const ScrollManager = (props) => {
       },
     });
   }, [section]);
-
-  useFrame(() => {
-    if (isAnimating.current) {
-      lastScroll.current = data.scroll.current;
-      return;
-    }
-
-    const curSection = Math.floor(data.scroll.current * data.pages);
-    if (data.scroll.current > lastScroll.current && curSection === 0) {
-      onSectionChange(1);
-    }
-    if (
-      data.scroll.current < lastScroll.current &&
-      data.scroll.current < 1 / (data.pages - 1)
-    ) {
-      onSectionChange(0);
-    }
-    lastScroll.current = data.scroll.current;
-  });
 
   return null;
 };
